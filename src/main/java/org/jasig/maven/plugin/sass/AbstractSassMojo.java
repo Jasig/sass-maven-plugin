085ff2c610d45755d6205cd06ba8b2170c1b1faa
Mark Prins <mprins@users.sf.net>
2013-02-27 00:31:40 +0100

    makes the plugin Compass aware
    Succesfully builds the sample project mprins/maven-compass-example.
    
    Note that for the build we're now relying on the Compass gem and the transitive dependency that has on the Sass gem.
    
    For the Mojo, since buildDirectory has a default value there is no sense in that being a required parameter.

--- src/main/java/org/jasig/maven/plugin/sass/AbstractSassMojo.java
+++ src/main/java/org/jasig/maven/plugin/sass/AbstractSassMojo.java
@@ -33,22 +33,23 @@ import org.apache.maven.plugin.logging.Log;
 import com.google.common.collect.ImmutableMap;
 
 /**
- * Base for batching SASS Mojos
+ * Base for batching SASS Mojos.
  * 
  */
 public abstract class AbstractSassMojo extends AbstractMojo {
 
     /**
-     * Sources for compilation with their destination directory containing SASS files
+     * Sources for compilation with their destination directory containing SASS files.
      *
      * @parameter
      * @required
      */
     protected List<Resource> resources;
-    
+
     /**
-     * @parameter expression="${encoding}" default-value="${project.build.directory}
-     * @required
+     * Build directory for the plugin.
+     *
+     * @parameter expression="${buildDirectory}" default-value="${project.build.directory}
      */
     protected File buildDirectory;
 
@@ -58,44 +59,65 @@ public abstract class AbstractSassMojo extends AbstractMojo {
      * @parameter default-value="true"
      */
     protected boolean failOnError;
-    
+
     /**
-     * Defines options for Sass::Plugin.options. See http://sass-lang.com/docs/yardoc/file.SASS_REFERENCE.html#options
+     * Defines options for Sass::Plugin.options. See
+     * {@link http://sass-lang.com/docs/yardoc/file.SASS_REFERENCE.html#options }
      * If the value is a string it must by quoted in the maven configuration:
-     * &lt;cache_location>'/tmp/sass'&lt;/cache_location>
-     * <br/>
+     * &lt;cache_location>'/tmp/sass'&lt;/cache_location> <br/>
      * If no options are set the default configuration set is used which is:
+     * 
+     * <pre>
      * &lt;unix_newlines>true&lt;/unix_newlines>
      * &lt;cache>true&lt;/cache>
      * &lt;always_update>true&lt;/always_update>
      * &lt;cache_location>${project.build.directory}/sass_cache&lt;/cache_location>
      * &lt;style>:expanded&lt;/style>
-     *
+     * </pre>
+     * 
      * @parameter
      */
     protected Map<String, String> sassOptions = new HashMap<String, String>(ImmutableMap.of(
-            "unix_newlines", "true", 
+            "unix_newlines", "true",
             "cache", "true",
             "always_update", "true",
             "style", ":expanded"));
 
-    
+    /**
+     * Enable the use of Compass style library mixins, this emulates the
+     * {@code --compass} commandline option of Sass.
+     *
+     * @parameter default-value="false"
+     */
+    protected boolean useCompass;
+
     protected void buildBasicSASSScript(final StringBuilder sassScript) throws MojoExecutionException {
         final Log log = this.getLog();
-        
+
         sassScript.append("require 'rubygems'\n");
         sassScript.append("require 'sass/plugin'\n");
         sassScript.append("require 'java'\n");
-        
+
+        if (this.useCompass) {
+            log.info("Running with Compass enabled.");
+            sassScript.append("require 'compass'\n");
+            sassScript.append("require 'compass/exec'\n");
+            sassScript.append("Compass.add_project_configuration \n");
+            this.sassOptions.put("load_paths", "Compass.configuration.sass_load_paths");
+            // manually specify these paths
+            sassScript.append("Compass::Frameworks.register_directory('jar:'+ File.join(Compass.base_directory, 'frameworks/compass'))\n");
+            sassScript.append("Compass::Frameworks.register_directory('jar:'+ File.join(Compass.base_directory, 'frameworks/blueprint'))\n");
+        }
+
         sassScript.append("Sass::Plugin.options.merge!(\n");
-        
+
         //If not explicitly set place the cache location in the target dir
         if (!this.sassOptions.containsKey("cache_location")) {
             final File sassCacheDir = new File(this.buildDirectory, "sass_cache");
             final String sassCacheDirStr = sassCacheDir.toString();
             this.sassOptions.put("cache_location", "'" + FilenameUtils.separatorsToUnix(sassCacheDirStr) + "'");
         }
-        
+
         //Add the plugin configuration options
         for (final Iterator<Entry<String, String>> entryItr = this.sassOptions.entrySet().iterator(); entryItr.hasNext();) {
             final Entry<String, String> optEntry = entryItr.next();
@@ -108,7 +130,7 @@ public abstract class AbstractSassMojo extends AbstractMojo {
             sassScript.append("\n");
         }
         sassScript.append(")\n");
-        
+
         // set up compilation error reporting
         sassScript.append("java_import ");
         sassScript.append(CompilationErrors.class.getName());
@@ -117,17 +139,22 @@ public abstract class AbstractSassMojo extends AbstractMojo {
         sassScript.append("Sass::Plugin.on_compilation_error {|error, template, css| $compilation_errors.add(template, error.message) }\n");
 
         //Add the SASS template locations
-        for (Resource source : resources) {
-        	for (Entry<String, String> entry : source.getDirectoriesAndDestinations().entrySet()) {
-        		log.info("Queing SASS Template for compile: " + entry.getKey() + " => " + entry.getValue());
+        for (final Resource source : this.resources) {
+            for (final Entry<String, String> entry : source.getDirectoriesAndDestinations().entrySet()) {
+                log.info("Queing SASS Template for compile: " + entry.getKey() + " => " + entry.getValue());
 
-    		    sassScript.append("Sass::Plugin.add_template_location('")
-    		    	.append(entry.getKey())
-    		    	.append("', '")
-    		    	.append(entry.getValue())
-    		    	.append("')\n");
-        	}
-		}
+                sassScript.append("Sass::Plugin.add_template_location('")
+                    .append(entry.getKey())
+                    .append("', '")
+                    .append(entry.getValue())
+                    .append("')\n");
+            }
+        }
+        // make ruby give use some debugging info when requested
+        if (log.isDebugEnabled()) {
+            sassScript.append("require 'pp'\npp Sass::Plugin.options\n");
+            sassScript.append("pp Compass::configuration\n");
+        }
     }
 
 }
