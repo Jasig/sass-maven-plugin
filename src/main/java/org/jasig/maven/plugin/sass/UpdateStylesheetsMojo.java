8cfa84f866422ac23838abd5b8f383c9bbe1c97d
Mark Paluch <mpaluch@paluch.biz>
2013-07-03 21:54:21 +0200

    Caching o improve build (if source did not change, do not perform sass script)

--- src/main/java/org/jasig/maven/plugin/sass/UpdateStylesheetsMojo.java
+++ src/main/java/org/jasig/maven/plugin/sass/UpdateStylesheetsMojo.java
@@ -18,8 +18,16 @@
  */
 package org.jasig.maven.plugin.sass;
 
+import com.google.common.io.Files;
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.plugin.Mojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.*;
+import java.util.zip.CRC32;
 
 /**
  * Mojo that compiles SASS Templates into CSS files. Uses JRuby to execute a generated script that calls the SASS GEM
@@ -27,12 +35,23 @@ import org.apache.maven.plugin.MojoFailureException;
  * @goal update-stylesheets
  * @phase process-sources
  */
-public class UpdateStylesheetsMojo extends AbstractSassMojo {
+public class UpdateStylesheetsMojo extends AbstractSassMojo
+{
 
     @Override
-    public void execute() throws MojoExecutionException, MojoFailureException {
+    public void execute() throws MojoExecutionException, MojoFailureException
+    {
         getLog().info("Compiling SASS Templates");
 
+
+        boolean execute = performExecutionCheck();
+
+
+        if (!execute)
+        {
+            getLog().info("Skipping SASS Templates, no changes");
+            return;
+        }
         // build sass script
         final StringBuilder sassBuilder = new StringBuilder();
         buildBasicSASSScript(sassBuilder);
@@ -42,4 +61,119 @@ public class UpdateStylesheetsMojo extends AbstractSassMojo {
         // ...and execute
         executeSassScript(sassScript);
     }
+
+    private boolean performExecutionCheck() throws MojoFailureException
+    {
+        boolean execute;
+
+        try
+        {
+            File cacheFile = constructChecksumFile();
+            Map<String, String> cachedChecksumMap = parseChecksumFile(cacheFile);
+            Map<String, String> currentChecksumMap = createCurrentChecksumMap();
+
+            if (currentChecksumMap.equals(cachedChecksumMap))
+            {
+                execute = false;
+            } else
+            {
+                execute = true;
+            }
+
+            writeChecksumFile(cacheFile, currentChecksumMap);
+
+        } catch (IOException e)
+        {
+            throw new MojoFailureException(e.getMessage(), e);
+        }
+        return execute;
+    }
+
+    private Map<String, String> createCurrentChecksumMap() throws IOException
+    {
+        Map<String, String> currentChecksumMap = new HashMap<String, String>();
+        Iterator<Map.Entry<String, String>> locations = getTemplateLocations();
+
+        while (locations.hasNext())
+        {
+            Map.Entry<String, String> entry = locations.next();
+            File sourceDir = new File(entry.getKey());
+
+            if (!sourceDir.exists())
+            {
+                continue;
+            }
+
+
+            Iterator<File> files = FileUtils.iterateFiles(sourceDir, new String[] { "scss" }, true);
+
+            while (files.hasNext())
+            {
+                File file = files.next();
+                String fileEntry = file.getCanonicalPath();
+                String currentChecksum = getChecksum(file);
+
+                currentChecksumMap.put(fileEntry, currentChecksum);
+            }
+        }
+        return currentChecksumMap;
+    }
+
+    private Map<String, String> parseChecksumFile(File cacheFile) throws IOException
+    {
+        Map<String, String> checksumMap = new HashMap<String, String>();
+
+        if (cacheFile.exists())
+        {
+            List<String> lines = Files.readLines(cacheFile, Charset.defaultCharset());
+
+            for (String line : lines)
+            {
+                if (!line.contains(" "))
+                {
+                    continue;
+                }
+
+                int index = line.indexOf(' ');
+
+                String checksum = line.substring(0, index);
+                String filename = line.substring(index + 1);
+                checksumMap.put(filename, checksum);
+            }
+        }
+        return checksumMap;
+    }
+
+    private void writeChecksumFile(File cacheFile, Map<String, String> checksumMap) throws FileNotFoundException
+    {
+        BufferedWriter writer = Files.newWriter(cacheFile, Charset.defaultCharset());
+        PrintWriter pw = new PrintWriter(writer);
+        for (Map.Entry<String, String> entry : checksumMap.entrySet())
+        {
+            pw.println(createChecksumLine(entry));
+        }
+        pw.close();
+    }
+
+    private String createChecksumLine(Map.Entry<String, String> entry)
+    {
+        return entry.getValue() + " " + entry.getKey();
+    }
+
+    private File constructChecksumFile()
+    {
+        File cacheDirectory = new File(buildDirectory, SASS_CACHE);
+        if (!cacheDirectory.exists())
+        {
+            cacheDirectory.mkdirs();
+        }
+        return new File(cacheDirectory, "checksums");
+    }
+
+    private String getChecksum(File file) throws IOException
+    {
+        long checksum = Files.getChecksum(file, new CRC32());
+        return Long.toHexString(checksum);
+    }
+
 }
